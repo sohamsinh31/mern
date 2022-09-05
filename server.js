184c2753864d048109d4bf314592ca3f9511b79a
sohamsinh31 <sohamb2019@gmail.com>
2022-09-05 11:59:22 +0530

    2.0

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const dev = process.env.NODE_ENV !== 'production'
 const hostname = 'localhost'
 const port = 3000
 
-const app = next({ dev, hostname, port })
+const app = next({dev})
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
